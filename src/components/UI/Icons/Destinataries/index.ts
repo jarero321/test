8916362b4e28ffbf31105521fa21b47a23198aea
Carlos Jarero <jareroluis@gmail.com>
2023-06-30 14:35:07 -0600

    feature/register-and-login
    Adding a api example with a models requireds for make a request

---
+++ src/components/UI/Icons/Destinataries/index.ts
@@ -0,0 +1,1 @@
+export { default as Destinataries } from './Destinataries';
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Authentication } from './pages/Actual/Authentication';
 import { Home } from './pages/Actual/Home';
 import { OnBoarding } from './pages/Legacy/UserRegister/OnBoarding';
 import Dashboard from '@/pages/Actual/DashBoard/Dashboard';
 import { ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute';
 import { useAuthStore } from './store/auth';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Router = () => {
   const isAuth = useAuthStore((state) => state.isAuth);
   return (
     <BrowserRouter>
+      <ToastContainer />
       <Routes>
+        <Route
+          element={
+            isAuth ? (
+              <Navigate to="/dashboard" />
+            ) : (
+              <Navigate to="/autenticacion/iniciar-sesion" />
+            )
+          }
+          path="/"
+        />
         <Route element={<Home />} path="/" />
         <Route element={<Authentication />} path="autenticacion/:step?" />
         <Route
