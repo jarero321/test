fcd515cbfd7b8ee08b776f07d1f7e3eb41969155
Diegoabustos <balorcode22@gmail.com>
2023-08-02 18:56:39 -0600

    feat: 🎸 Complete modal content

--- src/pages/Actual/DashBoard/SpeiPayments/containers/SpeiPaymentsForm/components/ConfirmPaymentModal/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ConfirmPaymentModal } from './ConfirmPaymentModal';
--- src/pages/Actual/DashBoard/SpeiPayments/containers/SpeiPaymentsForm/components/ProgramPaymentModal/ProgramPaymentModal.tsx
+++ src/pages/Actual/DashBoard/SpeiPayments/containers/SpeiPaymentsForm/components/ProgramPaymentModal/ProgramPaymentModal.tsx
@@ -1,6 +1,7 @@
 import { InputForm, Typography } from '@/components';
 import { ModalToken } from '@/components/UI/Modal/ModalToken';
 import SuccessIcon from '@/resources/icons/SuccessIcon';
+import { ListItem } from './components';
 
 interface ProgramPaymentModalProps {
   isOpen: boolean;
@@ -18,47 +19,28 @@ const ProgramPaymentModal: React.FC<ProgramPaymentModalProps> = ({
       isOpen={isOpen}
       onClose={onClose}
       title="Confirmación de retiro"
-      textButton="Actualizar"
+      textButton="Confirmar"
       navigate={onClose}
       content={
         <>
-          <div>
-            <Typography className="text-gray-600" colors="black" size="xs">
-              Nombre completo
-            </Typography>
-            <InputForm
-              type="text"
-              label=""
-              placeholder="Luis Miguel Saavedra Hernánez"
-              name="name"
-            />
-          </div>
-          <div className="mt-4">
-            <Typography className="text-gray-600" colors="black" size="xs">
-              Parentesco
-            </Typography>
-            <InputForm
-              type="text"
-              label=""
-              placeholder="Madre, Padre, Hijo, Esposa ..."
-              name="relationship"
-            />
-          </div>
-          <div className="mt-4">
-            <Typography className="text-gray-600" colors="black" size="xs">
-              Porcentaje
-            </Typography>
-            <InputForm
-              type="text"
-              label=""
-              placeholder="Defina un porcentaje"
-              name="percentage"
-            />
+          <div className="py-[24px] px-2 flex flex-col gap-[12px]">
+            <ListItem text="Gastón Ghietto" title="Nombre del Destinatario" />
+            <ListItem text="Número de celular" title="Tipo de cuenta" />
+            <ListItem text="**** **** **** 6709" title="Número de cuenta" />
+            <ListItem text="BBVA Bancomer" title="Institución Bancaria" />
+            <ListItem text="$46,948.00" title="Monto a retirar" />
+            <ListItem text="$2,347.40" title="Comisión" />
+            <ListItem text="$375.58" title="IVA" />
+            <ListItem text="$49,670.98" title="Total a retirar" />
+            <ListItem text="Pago activo" title="Concepto" />
+            <ListItem text="140723" title="Referencia" />
+            <ListItem text="14/07/2023" title="Fecha" />
+            <ListItem text="15:00 hrs" title="Hora" />
           </div>
 
           <div className="p-6 space-y-6 text-center">
             <Typography className="text-center" size="lg">
-              Token 2fa
+              Token
             </Typography>
             <div>
               <InputForm
@@ -76,9 +58,23 @@ const ProgramPaymentModal: React.FC<ProgramPaymentModalProps> = ({
           <div className="m-auto w-40 h-40">
             <SuccessIcon />
           </div>
-          <Typography className="text-center" colors="black" size="sm">
-            El beneficiario ha sido actualizado exitosamente
+          <Typography colors="grey" size="xs">
+            Retiro exitoso
           </Typography>
+          <div className="py-[24px] px-2 flex flex-col gap-[12px]">
+            <ListItem text="Gastón Ghietto" title="Nombre del Destinatario" />
+            <ListItem text="Número de celular" title="Tipo de cuenta" />
+            <ListItem text="**** **** **** 6709" title="Número de cuenta" />
+            <ListItem text="BBVA Bancomer" title="Institución Bancaria" />
+            <ListItem text="$46,948.00" title="Monto a retirar" />
+            <ListItem text="$2,347.40" title="Comisión" />
+            <ListItem text="$375.58" title="IVA" />
+            <ListItem text="$49,670.98" title="Total a retirar" />
+            <ListItem text="Pago activo" title="Concepto" />
+            <ListItem text="140723" title="Referencia" />
+            <ListItem text="14/07/2023" title="Fecha" />
+            <ListItem text="15:00 hrs" title="Hora" />
+          </div>
         </>
       }
     />
@@ -86,48 +82,27 @@ const ProgramPaymentModal: React.FC<ProgramPaymentModalProps> = ({
     <ModalToken
       isOpen={isOpen}
       onClose={onClose}
-      title="Confirmación de retiro 2"
-      textButton="Actualizar"
+      title="Confirmación de retiro"
+      textButton="Confirmar"
       navigate={onClose}
       content={
         <>
-          <div>
-            <Typography className="text-gray-600" colors="black" size="xs">
-              Nombre completo
-            </Typography>
-            <InputForm
-              type="text"
-              label=""
-              placeholder="Luis Miguel Saavedra Hernánez"
-              name="name"
-            />
-          </div>
-          <div className="mt-4">
-            <Typography className="text-gray-600" colors="black" size="xs">
-              Parentesco
-            </Typography>
-            <InputForm
-              type="text"
-              label=""
-              placeholder="Madre, Padre, Hijo, Esposa ..."
-              name="relationship"
-            />
-          </div>
-          <div className="mt-4">
-            <Typography className="text-gray-600" colors="black" size="xs">
-              Porcentaje
-            </Typography>
-            <InputForm
-              type="text"
-              label=""
-              placeholder="Defina un porcentaje"
-              name="percentage"
-            />
+          <div className="py-[24px] px-2 flex flex-col gap-[12px]">
+            <ListItem text="Gastón Ghietto" title="Nombre del Destinatario" />
+            <ListItem text="Número de celular" title="Tipo de cuenta" />
+            <ListItem text="**** **** **** 6709" title="Número de cuenta" />
+            <ListItem text="BBVA Bancomer" title="Institución Bancaria" />
+            <ListItem text="$46,948.00" title="Monto a retirar" />
+            <ListItem text="$2,347.40" title="Comisión" />
+            <ListItem text="$375.58" title="IVA" />
+            <ListItem text="$49,670.98" title="Total a retirar" />
+            <ListItem text="Pago activo" title="Concepto" />
+            <ListItem text="140723" title="Referencia" />
           </div>
 
           <div className="p-6 space-y-6 text-center">
             <Typography className="text-center" size="lg">
-              Token 2fa
+              Token
             </Typography>
             <div>
               <InputForm
@@ -145,9 +120,21 @@ const ProgramPaymentModal: React.FC<ProgramPaymentModalProps> = ({
           <div className="m-auto w-40 h-40">
             <SuccessIcon />
           </div>
-          <Typography className="text-center" colors="black" size="sm">
-            El beneficiario ha sido actualizado exitosamente
+          <Typography colors="grey" size="xs">
+            Retiro exitoso
           </Typography>
+          <div className="py-[24px] px-2 flex flex-col gap-[12px]">
+            <ListItem text="Gastón Ghietto" title="Nombre del Destinatario" />
+            <ListItem text="Número de celular" title="Tipo de cuenta" />
+            <ListItem text="**** **** **** 6709" title="Número de cuenta" />
+            <ListItem text="BBVA Bancomer" title="Institución Bancaria" />
+            <ListItem text="$46,948.00" title="Monto a retirar" />
+            <ListItem text="$2,347.40" title="Comisión" />
+            <ListItem text="$375.58" title="IVA" />
+            <ListItem text="$49,670.98" title="Total a retirar" />
+            <ListItem text="Pago activo" title="Concepto" />
+            <ListItem text="140723" title="Referencia" />
+          </div>
         </>
       }
     />
